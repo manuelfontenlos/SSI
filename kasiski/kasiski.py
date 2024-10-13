ad5d913281d1bdd70768c96df5d26d59e652f5ac
manuelfontenlos <manuel.fontenlos@udc.es>
2024-10-13 22:00:25 +0200

    el algoritmo obtiene las letras mas frecuentes de cada fragmento

--- kasiski/kasiski.py
+++ kasiski/kasiski.py
@@ -1,15 +1,19 @@
 import argparse
-
-#listas para guardar las apariciones de cada subcriptograma
-cuatrihashmapcount = {}
-trihashmapcount = {}
+import itertools
 
 #listas para guardar la distancia entre cada subcriptograma
 cuatrihashmapdiff = {}
 trihashmapdiff = {}
 
 # Inicializa el diccionario para contar los múltiplos
-mcdhashmap = {i: 0 for i in range(2, 7)}
+mcdhashmap = {i: 0 for i in range(2, 8)}
+
+fragmentos = []
+
+frecuencias = []
+
+maxletter = {}
+maxletter2 = {}
 
 def buscar_cuatrigramas(text):
     for index in range(0,len(text)-4):
@@ -20,9 +24,8 @@ def buscar_cuatrigramas(text):
                 auxcuatrigram = text[auxindex:auxindex+4]
                 if cuatrigram == auxcuatrigram:
                     count+=1
-                    cuatrihashmapcount[cuatrigram] = count
                     cuatrihashmapdiff[cuatrigram] = auxindex - index
-                    print(f"Cuatrigrama {cuatrigram} repetido {count} veces, diferencia {auxindex-index}")
+                    #print(f"Cuatrigrama {cuatrigram} repetido {count} veces, diferencia {auxindex-index}")
 
 
 def buscar_trigramas(text):
@@ -34,21 +37,77 @@ def buscar_trigramas(text):
                 auxtrigram = text[auxindex:auxindex+3]
                 if trigram == auxtrigram:
                     count+=1
-                    trihashmapcount[trigram] = count
                     trihashmapdiff[trigram] = auxindex - index
-                    print(f"Trigrama {trigram} repetido {count} veces, diferencia {auxindex-index}")
+                    #print(f"Trigrama {trigram} repetido {count} veces, diferencia {auxindex-index}")
 
 
 def calcular_mcd():
-    for i in range(2,7):
+    for i in range(2,8):
         for cuatrigram in cuatrihashmapdiff:
             if(cuatrihashmapdiff[cuatrigram] % i == 0):
                 mcdhashmap[i] += 1
-    for i in range(2,7):
         for trigram in trihashmapdiff:
             if(trihashmapdiff[trigram] % i == 0):
                 mcdhashmap[i] += 1
-            
+
+    #print("La longitud de la clave es el máximo común divisor más repetido:")    
+    #for i in range(2,8):
+        #print(f"{i}: {mcdhashmap[i]}")
+
+    max=0
+    max_mcd=0
+    for i in range(2,8):
+        if mcdhashmap[i]>max:
+            max=mcdhashmap[i]
+            max_mcd=i
+    
+    return max_mcd
+
+
+#División del texto en fragmentos cifrados con la misma letra
+def dividir_fragmentos(text,mcd):
+    l = len(text)
+    for i in range(mcd):
+        fragmentos.append([])
+
+    for i in range(mcd):
+        for j in range(i,l,5):
+            if j < l:
+                fragmentos[i].append(text[j])
+
+
+#Comprobamos las letras más repetidas en cada fragmento
+def analisis_fragmentos(mcd):
+
+    for i in range(mcd):
+        frecuencias.append({'A': 0.0, 'B': 0.0, 'C': 0.0, 'D': 0.0, 
+            'E': 0.0, 'F': 0.0, 'G': 0.0, 'H': 0.0, 'I': 0.0, 'J': 0.0, 
+            'K': 0.0, 'L': 0.0, 'M': 0.0, 'N': 0.0, 'O': 0.0, 'P': 0.0, 
+            'Q': 0.0, 'R': 0.0, 'S': 0.0, 'T': 0.0, 'U': 0.0, 'V': 0.0, 
+            'W': 0.0, 'X': 0.0, 'Y': 0.0, 'Z': 0.0}) 
+    
+    for i in range(mcd):
+        for key in frecuencias[i].keys(): # Coge de la A-Z
+            for letter in fragmentos[i]: # Coge las letras del subcriptograma
+                if letter == key: # Si coinciden se suma 1
+                    frecuencias[i][key] += 1
+
+
+    #Calculamos la letra más repetida en el fragmento
+    for i in range(mcd):
+        maxfrec=0
+        for j in frecuencias[i].keys():
+            if frecuencias[i][j] > maxfrec:
+                maxletter[i] = str(j)
+                maxfrec = frecuencias[i][j]
+    
+    for i in range(mcd):
+        maxfrec=0
+        for j in frecuencias[i].keys():
+            if str(j) != maxletter[i]  :
+                if frecuencias[i][j] > maxfrec:
+                    maxletter2[i] = str(j)
+                    maxfrec = frecuencias[i][j]
 
 def main():
     parser = argparse.ArgumentParser(description='Análisis de cifrado vigenere con kasiski')
@@ -62,10 +121,15 @@ def main():
 
     buscar_cuatrigramas(mensaje)
     buscar_trigramas(mensaje)
-    calcular_mcd()
+    
+    mcd = calcular_mcd()
 
-    for i in range(2,7):
-        print(f"{i}: {mcdhashmap[i]}")
+    dividir_fragmentos(mensaje,mcd)
+    analisis_fragmentos(mcd)
+    
+    for i in range(mcd):
+        print(f"Las letras más repetidas en el fragmento {i} son las letras: {maxletter[i]}, {maxletter2[i]}")
+
 
 if __name__ == "__main__":
     main()
